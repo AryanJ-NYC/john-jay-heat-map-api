258e5236654b2cd710d993239e2c904f587dbcee
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-10-24 15:00:37 -0400

    Format time objects in api/weather endpoint

--- routes/weather/weather.routes.js
+++ routes/weather/weather.routes.js
@@ -2,16 +2,35 @@
 const router = require('express').Router(),
       squel = require('squel'),
       database = require('../../config/database.js'),
-      connection = database.connection,
-      DB_LIMIT = database.LIMIT;
+      connection = database.connection;
 
 router.get('/', function (req, res) {
-  let weatherQuery = squel.select().from('weather').limit(DB_LIMIT).toString();
+  let weatherQuery = squel.select()
+                          .from('weather')
+                          .field('id')
+                          .field('stime')
+                          .field('temp')
+                          .toString();
   connection.query(weatherQuery, function (err, rows) {
     if (err) throw err;
+
+    rows.map(function (tempObj) {
+      tempObj.stime = formatTime(tempObj.stime);
+      return tempObj;
+    });
 
     res.json(rows);
   });
 });
+
+function formatTime (date) {
+  let dateObj = new Date(date);
+  let year = dateObj.getFullYear();
+  let month = (dateObj.getMonth() + 1 < 10) ? `0${dateObj.getMonth()+1}` : dateObj.getMonth()+1;
+  let day = (dateObj.getDate() < 10) ? `0${dateObj.getDate()}` : dateObj.getDate();
+  let hour = (dateObj.getHours() < 10) ? `0${dateObj.getHours()}` : dateObj.getHours();
+
+  return `${year}-${month}-${day} ${hour}:00:00`;
+}
 
 module.exports = router;
