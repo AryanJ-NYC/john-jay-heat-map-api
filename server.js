88771fb432b04167f7b24e998543f14f665c4167
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-11-10 19:21:48 -0500

    Allow cross-origin HTTP requests

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 'use strict';
 const express = require('express'),
       app = express(),
+      cors = require('cors'),
       database = require('./config/database.js'),
       connection = database.connection,
       DB_LIMIT = database.LIMIT,
       port = process.env.PORT || 8000;
 
+app.use(cors());
+
+// error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: { message: err.message }});
